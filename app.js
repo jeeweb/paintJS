cf0fd80e568731a4558e4aa93f8d7a130995bbb5
jiee <jee.vvvv@gmail.com>
2021-08-30 14:02:45 +0900

    painting(그리기) / Filling(전체칠하기) 기능 추가

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.querySelectorAll(".jsColor");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_WIDTH = 700;
+const CANVAS_HEIGHT = 500;
 
 let painting = false;
+let filling = false;
 
-canvas.width = 700;
-canvas.height = 500;
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_WIDTH
+canvas.height = CANVAS_HEIGHT;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 function stopPainting() {
@@ -38,6 +46,7 @@ function onMouseDown(event) {
 function handleColorClick(event) {
 	const color = event.target.style.backgroundColor;
 	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -45,15 +54,37 @@ function handleRangeChange(event) {
 	ctx.lineWidth = stroke;	
 }
 
+function handleModeClick() {
+	if(filling === true) {
+		filling = false;
+		mode.innerText = "Fill"
+	} else {
+		filling = true;
+		mode.innerText = "Paint";
+	}
+}
+
+function handleCanvasClick() {
+	if(filling) {
+		ctx.fillRect(0, 0, canvas.width, canvas.height)
+	}
+}
+
+
 if(canvas){
 	canvas.addEventListener("mousemove", onMouseMove);
 	canvas.addEventListener("mousedown", startPainting);
 	canvas.addEventListener("mouseup", stopPainting);
 	canvas.addEventListener("mouseleave", stopPainting);
+	canvas.addEventListener("click", handleCanvasClick);
 }
 
 colors.forEach(color => color.addEventListener("click", handleColorClick));
 
 if(range){
 	range.addEventListener("input", handleRangeChange)
+}
+
+if(mode){
+	mode.addEventListener("click", handleModeClick)
 }
