be0c54f814d69b693c82985eb393cf2b9a768e39
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-10-24 23:23:26 +0900

    drawing lines

--- app.js
+++ app.js
@@ -3,22 +3,29 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 사람그리기
+ctx.lineWidth = 2;
+//ctx.moveTo(0, 0); // 0,0 부터 시작
 
-// 팔
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+]
 
-// 몸통
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+function onClick(event) {
+  // console.log(event);
+  // 내가 찍은 값의 좌표는 offsetX, offsetY 값으로 알 수 있음
+  ctx.beginPath(); // 그리는 선마다 path를 다르게 해서 색상이 다르게 표현됨
+  ctx.moveTo(0, 0);  // 클릭할 때마다 0,0 부터 시작
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
 
-// 머리
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill()
-
-// 눈
-ctx.beginPath();
-ctx.fillStyle = "white"
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+// canvas.addEventListener('click', onClick);
+canvas.addEventListener('mousemove', onClick);
