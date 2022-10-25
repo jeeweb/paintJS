9930819acafc1a626bcec7128f47175901d31185
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-10-25 23:54:11 +0900

    mouseDown으로 그리기

--- app.js
+++ app.js
@@ -4,28 +4,26 @@ canvas.width = 800;
 canvas.height = 800;
 
 ctx.lineWidth = 2;
-//ctx.moveTo(0, 0); // 0,0 부터 시작
+let isPainting = false;
 
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-]
-
-function onClick(event) {
-  // console.log(event);
-  // 내가 찍은 값의 좌표는 offsetX, offsetY 값으로 알 수 있음
-  ctx.beginPath(); // 그리는 선마다 path를 다르게 해서 색상이 다르게 표현됨
-  ctx.moveTo(0, 0);  // 클릭할 때마다 0,0 부터 시작
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function onMove(event) {
+  if(isPainting){
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-// canvas.addEventListener('click', onClick);
-canvas.addEventListener('mousemove', onClick);
+function startPainting() {
+  isPainting = true;
+}
+
+function cancelPainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener('mousemove', onMove);
+canvas.addEventListener('mousedown', startPainting);
+canvas.addEventListener('mouseup', cancelPainting);
+canvas.addEventListener('mouseleave', cancelPainting);
