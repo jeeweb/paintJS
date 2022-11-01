b3079e58032eb9d12ad4711511abc8eb096eaad8
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-11-01 23:01:28 +0900

    adding filling mode - 캔버스 전체 색 칠하기

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
-//const colorOptions = document.getElementsByClassName("color-option");
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
-// HTML Collection을 자바스크립트 배열로 만들어줌
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
   if(isPainting){
@@ -51,12 +50,31 @@ function onColorClick(event){
   color.value = colorValue; // 선택한 색상을 input에 표시
 }
 
-canvas.addEventListener('mousemove', onMove);
-canvas.addEventListener('mousedown', startPainting);
-canvas.addEventListener('mouseup', cancelPainting);
-canvas.addEventListener('mouseleave', cancelPainting);
+function onModeClick(){
+  if(isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCavasClick() {
+  if(isFilling){
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCavasClick);
 
 lineWidth.addEventListener('change', onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
