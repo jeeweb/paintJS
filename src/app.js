f89f66b6eb917fdd70a00b4307e399a05eacffd5
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-04 23:57:30 +0900

    폴더구조 변경 및 스타일 수정

--- src/app.js
+++ src/app.js
@@ -4,24 +4,26 @@ const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
-const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const CANVAS_WIDTH = 800;
+const CANVAS_WIDTH = 1000;
 const CANVAS_HEIGHT = 800;
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
-ctx.lineCap = "round"
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
 function onMove(event) {
-  if(isPainting){
+  if (isPainting) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
     return;
@@ -49,29 +51,36 @@ function onColorChange(event) {
   ctx.fillStyle = event.target.value;
 }
 
-function onColorClick(event){
+function onColorClick(event) {
   // 어떤 색상이 선택되었는지 알아보기
   // console.log(event.target);  // 선택된 html 태그를 보여줌
   // console.dir(event.target);  // 선택된 div의 관련 프로퍼티를 보여줌
   // console.log(event.target.dataset.color); //선택된 색상값
-  const colorValue = event.target.dataset.color; 
+  const colorValue = event.target.dataset.color;
   ctx.strokeStyle = colorValue;
   ctx.fillStyle = colorValue;
   color.value = colorValue; // 선택한 색상을 input에 표시
 }
 
-function onModeClick(){
-  if(isFilling) {
+function onModeClick() {
+  const modeBtnIcon = modeBtn.querySelector("i.fas");
+  const modeBtnTxt = modeBtn.querySelector("span");
+
+  if (isFilling) {
     isFilling = false;
-    modeBtn.innerText = "Fill";
+    modeBtnIcon.classList.remove("fa-paint-brush");
+    modeBtnIcon.classList.add("fa-fill");
+    modeBtnTxt.innerText = "Fill";
   } else {
     isFilling = true;
-    modeBtn.innerText = "Draw";
+    modeBtnIcon.classList.remove("fa-fill");
+    modeBtnIcon.classList.add("fa-paint-brush");
+    modeBtnTxt.innerText = "Draw";
   }
 }
 
 function onCavasClick() {
-  if(isFilling){
+  if (isFilling) {
     ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   }
 }
@@ -91,29 +100,30 @@ function onFileChange(event) {
   // console.dir(event.target);
   const file = event.target.files[0];
   const url = URL.createObjectURL(file);
-  // console.log(url);   // 유저가 선택한 이미지의 URL을 만들어 줌 
-  const image = new Image();  // document.createElement("img");
+  // console.log(url);   // 유저가 선택한 이미지의 URL을 만들어 줌
+  const image = new Image(); // document.createElement("img");
   image.src = url;
-  image.onload = function() {
+  image.onload = function () {
     ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     fileInput.value = null; // file input을 비워줌
-  }
+  };
 }
 
 function onDoubleClick(event) {
   const text = textInput.value;
 
-  if (text !== "") {  // input에 어떠한 text도 없으면 아무일도 일어나지 않게 하기
+  if (text !== "") {
+    // input에 어떠한 text도 없으면 아무일도 일어나지 않게 하기
     ctx.save(); // ctx의 현재상태, 색상, 스타일 등 모든것을 저장해줌
     // console.log(event.offsetX, event.offsetY);  // 마우스가 클릭한 지점의 좌표값
-    
-    ctx.lineWidth = 1;  // 글씨가 잘 보이게 하기 위해 lineWidth조정
+
+    ctx.lineWidth = 1; // 글씨가 잘 보이게 하기 위해 lineWidth조정
     ctx.font = "48px serif"; // 글씨 size와 fon-family를 지정할 수 있음
     // ctx.strokeText(text, event.offsetX, event.offsetY);  // 글씨 테두리만 보여줌
     ctx.fillText(text, event.offsetX, event.offsetY);
-    
+
     //글씨를 보여준 후 기존에 가지고 있던 lineWidth로 돌아가야함
-    ctx.restore();  // save 했던 시점으로 돌아감 -> save와 restore 사이에서의 수정은 저장 되지 않음
+    ctx.restore(); // save 했던 시점으로 돌아감 -> save와 restore 사이에서의 수정은 저장 되지 않음
   }
 }
 
@@ -136,10 +146,10 @@ canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 canvas.addEventListener("click", onCavasClick);
 
-lineWidth.addEventListener('change', onLineWidthChange);
+lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
-colorOptions.forEach(color => color.addEventListener("click", onColorClick));
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
